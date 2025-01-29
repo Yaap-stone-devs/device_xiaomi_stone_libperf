6da14e14b63d11ca9ee93746cae3bafe100bd44e
Mayuresh Nanal <92848611+Mayuresh2543@users.noreply.github.com>
2025-01-29 11:12:25 +0000

    Reapply "Add back telephony stuff but a bit fixed"
    This reverts commit 3260eb7ae0517cb9e8a0dcd38eefe3ad14c9546f.

---
+++ libinit/init_stone.cpp
@@ -0,0 +1,105 @@
+/*
+ * Copyright (C) 2021-2022 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <libinit_dalvik_heap.h>
+#include <libinit_variant.h>
+#include <libinit_utils.h>
+
+#include "vendor_init.h"
+
+#define FINGERPRINT_MOON_GL "POCO/moonstone_p_global/moonstone:14/UKQ1.231003.002/V816.0.13.0.UMPMIXM:user/release-keys"
+#define FINGERPRINT_MOON_IN "POCO/moonstone/moonstone:14/UKQ1.231003.002/V816.0.10.0.UMPINXM:user/release-keys"
+#define FINGERPRINT_SUN_GL "Redmi/sunstone_global/sunstone:14/UKQ1.230917.001/V816.0.9.0.UMQMIXM:user/release-keys"
+#define FINGERPRINT_SUN_IN "Redmi/sunstone/sunstone:14/UKQ1.230917.001/V816.0.6.0.UMQINXM:user/release-keys"
+#define FINGERPRINT_SUN_CN "Redmi/sunstone/sunstone:14/UKQ1.230917.001/V816.0.4.0.UMQCNXM:user/release-keys"
+
+static const variant_info_t stone_global_info = {
+    .hwc_value = "",
+    .sku_value = "moonstone_p_global",
+
+    .brand = "POCO",
+    .device = "moonstone",
+    .model = "POCO X5 5G",
+    .mod_device = "moonstone_p_global",
+    .build_fingerprint = FINGERPRINT_MOON_GL,
+};
+
+static const variant_info_t stone_in_info = {
+    .hwc_value = "",
+    .sku_value = "moonstone_p_in",
+
+    .brand = "POCO",
+    .device = "moonstone",
+    .model = "POCO X5 5G",
+//  .mod_device = "moonstone_in_global",
+    .mod_device = "moonstone_p_global", // Fixed miuicamera crash
+    .build_fingerprint = FINGERPRINT_MOON_IN,
+};
+
+static const variant_info_t sunstone_cn_info = {
+    .hwc_value = "",
+    .sku_value = "sunstone_cn",
+
+    .brand = "Redmi",
+    .device = "sunstone",
+    .model = "Redmi Note 12 5G",
+//  .mod_device = "moonstone",
+    .mod_device = "moonstone_global", // Fixed miuicamera crash
+    .build_fingerprint = FINGERPRINT_SUN_CN,
+};
+
+static const variant_info_t sunstone_global_info = {
+    .hwc_value = "",
+    .sku_value = "sunstone_global",
+
+    .brand = "Redmi",
+    .device = "sunstone",
+    .model = "Redmi Note 12 5G",
+//  .mod_device = "moonstone",
+    .mod_device = "moonstone_global", // Fixed miuicamera crash
+    .build_fingerprint = FINGERPRINT_SUN_GL,
+};
+
+static const variant_info_t sunstone_in_info = {
+    .hwc_value = "",
+    .sku_value = "sunstone_in",
+
+    .brand = "Redmi",
+    .device = "sunstone",
+    .model = "Redmi Note 12 5G",
+//  .mod_device = "moonstone",
+    .mod_device = "moonstone_global", // Fixed miuicamera crash
+    .build_fingerprint = FINGERPRINT_SUN_IN,
+};
+
+static const variant_info_t sunstone_x_info = {
+    .hwc_value = "",
+    .sku_value = "sunstone_x",
+
+    .brand = "Redmi",
+    .device = "sunstone",
+    .model = "Redmi Note 12R Pro",
+//  .mod_device = "moonstone",
+    .mod_device = "moonstone_global", // Fixed miuicamera crash
+    .build_fingerprint = FINGERPRINT_SUN_CN,
+};
+
+static const std::vector<variant_info_t> variants = {
+    stone_global_info,
+    stone_in_info,
+    sunstone_global_info,
+    sunstone_cn_info,
+    sunstone_in_info,
+    sunstone_x_info,
+};
+
+void vendor_load_properties() {
+    set_dalvik_heap();
+    search_variant(variants);
+
+    // SafetyNet workaround
+    property_override("ro.boot.verifiedbootstate", "green");
+}
---
+++ libinit/libinit_dalvik_heap.cpp
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <sys/sysinfo.h>
+#include <libinit_utils.h>
+
+#include <libinit_dalvik_heap.h>
+
+#define HEAPSTARTSIZE_PROP "dalvik.vm.heapstartsize"
+#define HEAPGROWTHLIMIT_PROP "dalvik.vm.heapgrowthlimit"
+#define HEAPSIZE_PROP "dalvik.vm.heapsize"
+#define HEAPMINFREE_PROP "dalvik.vm.heapminfree"
+#define HEAPMAXFREE_PROP "dalvik.vm.heapmaxfree"
+#define HEAPTARGETUTILIZATION_PROP "dalvik.vm.heaptargetutilization"
+
+#define GB(b) (b * 1024ull * 1024 * 1024)
+
+static const dalvik_heap_info_t dalvik_heap_info_8192 = {
+    .heapstartsize = "24m",
+    .heapgrowthlimit = "256m",
+    .heapsize = "512m",
+    .heapminfree = "8m",
+    .heapmaxfree = "48m",
+    .heaptargetutilization = "0.46",
+};
+
+static const dalvik_heap_info_t dalvik_heap_info_6144 = {
+    .heapstartsize = "16m",
+    .heapgrowthlimit = "256m",
+    .heapsize = "512m",
+    .heapminfree = "8m",
+    .heapmaxfree = "32m",
+    .heaptargetutilization = "0.5",
+};
+
+static const dalvik_heap_info_t dalvik_heap_info_4096 = {
+    .heapstartsize = "8m",
+    .heapgrowthlimit = "192m",
+    .heapsize = "512m",
+    .heapminfree = "8m",
+    .heapmaxfree = "16m",
+    .heaptargetutilization = "0.6",
+};
+
+void set_dalvik_heap() {
+    struct sysinfo sys;
+    const dalvik_heap_info_t *dhi;
+
+    sysinfo(&sys);
+
+    if (sys.totalram > GB(7))
+        dhi = &dalvik_heap_info_8192;
+    else if (sys.totalram > GB(5))
+        dhi = &dalvik_heap_info_6144;
+    else
+        dhi = &dalvik_heap_info_4096;
+
+    /* Set props for 4GB Users */
+    if (sys.totalram < GB(5)) {
+	property_override("ro.config.avoid_gfx_accel", "true");
+	property_override("ro.config.fha_enable", "true");
+	property_override("ro.sys.fw.bg_apps_limit", "32");
+	property_override("ro.config.dha_cached_max", "16");
+	property_override("ro.config.dha_empty_max", "42");
+	property_override("ro.config.dha_empty_init", "32");
+	property_override("ro.config.dha_lmk_scale", "0.545");
+	property_override("ro.config.dha_th_rate", "2.3");
+	property_override("ro.config.sdha_apps_bg_max", "64");
+	property_override("ro.config.sdha_apps_bg_min", "8");
+        }    
+
+    property_override(HEAPSTARTSIZE_PROP, dhi->heapstartsize);
+    property_override(HEAPGROWTHLIMIT_PROP, dhi->heapgrowthlimit);
+    property_override(HEAPSIZE_PROP, dhi->heapsize);
+    property_override(HEAPTARGETUTILIZATION_PROP, dhi->heaptargetutilization);
+    property_override(HEAPMINFREE_PROP, dhi->heapminfree);
+    property_override(HEAPMAXFREE_PROP, dhi->heapmaxfree);
+}
