554d213a6d86e8eedb1bce22b92e87a887efe449
Mayuresh Nanal <92848611+Mayuresh2543@users.noreply.github.com>
2025-01-30 18:04:30 +0530

    stone: add back some telephony stuff

--- device.mk
+++ device.mk
@@ -539,7 +539,9 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/qcom-caf/common/libqti-perfd-client
 
 # Telephony
-
+PRODUCT_PACKAGES += \
+    xiaomi-telephony-stub
+    
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
