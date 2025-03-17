ebeb5590e816cea09163435a8474ffbaf84db0ff
Khnome <75486623+Khnome@users.noreply.github.com>
2025-03-17 19:08:14 +0200

    stone: Enabežle Vulkan UI Renderer

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -250,5 +250,9 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 # Treble
 BOARD_VNDK_VERSION := current
 
+# Vulkan
+#
+# Vulcan
+TARGET_USES_VULKAN := true
 # Inherit the proprietary files
 include vendor/xiaomi/stone/BoardConfigVendor.mk
